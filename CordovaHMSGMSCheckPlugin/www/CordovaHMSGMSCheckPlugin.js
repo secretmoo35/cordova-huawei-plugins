0b6a950dac3bc9c23c78e12d487622ccecbb72ea
Sarayut Kungsaranuwat <sarayut@indigy.com>
2022-09-28 17:41:06 +0700

    Update

--- CordovaHMSGMSCheckPlugin/www/CordovaHMSGMSCheckPlugin.js
+++
@@ -1,9 +0,0 @@
-var exec = require("cordova/exec");
-
-exports.isHmsAvailable = function (arg0, success, error) {
-  exec(success, error, "CordovaHMSGMSCheckPlugin", "isHmsAvailable", [arg0]);
-};
-
-exports.isGmsAvailable = function (arg0, success, error) {
-  exec(success, error, "CordovaHMSGMSCheckPlugin", "isGmsAvailable", [arg0]);
-};
